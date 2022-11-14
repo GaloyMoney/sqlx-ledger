c177fcf92d39c9a515f406e92ee838d788906115
bodymindarts <justin@galoy.io>
2022-11-14 09:42:42 +0100

    refactor: fix parser

--- cel-parser/src/ast.rs
+++ cel-parser/src/ast.rs
@@ -1,6 +1,12 @@
 use std::rc::Rc;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
+pub enum LogicOp {
+    And,
+    Or,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub enum RelationOp {
     LessThan,
     LessThanEq,
@@ -28,23 +34,44 @@ pub enum UnaryOp {
     DoubleMinus,
 }
 
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum Operation {
+    Logic(LogicOp),
+    Relation(RelationOp),
+    Arithmetic(ArithmeticOp),
+    Unary(UnaryOp),
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expression {
-    Arithmetic(Box<Expression>, ArithmeticOp, Box<Expression>),
-    Relation(Box<Expression>, RelationOp, Box<Expression>),
-
     Ternary(Box<Expression>, Box<Expression>, Box<Expression>),
     Or(Box<Expression>, Box<Expression>),
     And(Box<Expression>, Box<Expression>),
+    Relation(RelationOp, Box<Expression>, Box<Expression>),
+    Arithmetic(ArithmeticOp, Box<Expression>, Box<Expression>),
     Unary(UnaryOp, Box<Expression>),
 
     Member(Box<Expression>, Box<Member>),
 
     List(Vec<Expression>),
     Map(Vec<(Expression, Expression)>),
+    Struct(Vec<Rc<String>>, Vec<(Rc<String>, Expression)>),
 
-    Atom(Atom),
+    Literal(Literal),
     Ident(Rc<String>),
+}
+
+impl Expression {
+    pub(crate) fn from_op(op: Operation, left: Box<Expression>, right: Box<Expression>) -> Self {
+        use Operation::*;
+        match op {
+            Logic(LogicOp::Or) => Expression::Or(left, right),
+            Logic(LogicOp::And) => Expression::And(left, right),
+            Relation(op) => Expression::Relation(op, left, right),
+            Arithmetic(op) => Expression::Arithmetic(op, left, right),
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -52,14 +79,13 @@ pub enum Member {
     Attribute(Rc<String>),
     FunctionCall(Vec<Expression>),
     Index(Box<Expression>),
-    Fields(Vec<(Rc<String>, Expression)>),
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Atom {
-    Int(i32),
-    UInt(u32),
-    Float(f64),
+pub enum Literal {
+    Int(i64),
+    UInt(u64),
+    Double(f64),
     String(Rc<String>),
     Bytes(Rc<Vec<u8>>),
     Bool(bool),
@@ -69,7 +95,7 @@ pub enum Atom {
 #[cfg(test)]
 mod tests {
     use crate::parser::ExpressionParser;
-    use crate::{Atom::*, Expression, Expression::*, Member::*};
+    use crate::{ArithmeticOp::*, Expression, Expression::*, Literal::*, Member::*};
 
     fn parse(input: &str) -> Expression {
         ExpressionParser::new()
@@ -82,28 +108,69 @@ mod tests {
     }
 
     #[test]
-    fn simple_int() {
-        assert_parse_eq("1", Atom(Int(1)))
-    }
-
-    #[test]
-    fn simple_float() {
-        assert_parse_eq("1.0", Atom(Float(1.0)))
-    }
-
-    #[test]
-    fn nested_attributes() {
+    fn logic() {
         assert_parse_eq(
-            "a.b[1]",
-            Member(
-                Member(
-                    Ident("a".to_string().into()).into(),
-                    Attribute("b".to_string().into()).into(),
-                )
-                .into(),
-                Index(Atom(Int(1)).into()).into(),
+            "true || false && true",
+            Or(
+                Literal(Bool(true)).into(),
+                And(Literal(Bool(false)).into(), Literal(Bool(true)).into()).into(),
+            ),
+        )
+    }
+    #[test]
+    fn op_precendence() {
+        assert_parse_eq(
+            "1 + 2 * 3",
+            Arithmetic(
+                Add,
+                Literal(Int(1)).into(),
+                Arithmetic(Multiply, Literal(Int(2)).into(), Literal(Int(3)).into()).into(),
+            )
+            .into(),
+        );
+        assert_parse_eq(
+            "1 * 2 + 3",
+            Arithmetic(
+                Add,
+                Arithmetic(Multiply, Literal(Int(1)).into(), Literal(Int(2)).into()).into(),
+                Literal(Int(3)).into(),
+            )
+            .into(),
+        );
+        assert_parse_eq(
+            "1 * (2 + 3)",
+            Arithmetic(
+                Multiply,
+                Literal(Int(1)).into(),
+                Arithmetic(Add, Literal(Int(2)).into(), Literal(Int(3)).into()).into(),
             )
             .into(),
         )
     }
+
+    #[test]
+    fn simple_int() {
+        assert_parse_eq("1", Literal(Int(1)))
+    }
+
+    #[test]
+    fn simple_float() {
+        assert_parse_eq("1.0", Literal(Double(1.0)))
+    }
+
+    // #[test]
+    // fn nested_attributes() {
+    //     assert_parse_eq(
+    //         "a.b[1]",
+    //         Member(
+    //             Member(
+    //                 Ident("a".to_string().into()).into(),
+    //                 Attribute("b".to_string().into()).into(),
+    //             )
+    //             .into(),
+    //             Index(Literal(Int(1)).into()).into(),
+    //         )
+    //         .into(),
+    //     )
+    // }
 }
